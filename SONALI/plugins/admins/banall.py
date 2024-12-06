db33a8e637f0f7245a50cc6bf0e4e673b02332de
Radhe0777 <princek8356@gmail.com>
2024-12-06 13:50:33 +0530

    Update banall.py

--- SONALI/plugins/admins/banall.py
+++ SONALI/plugins/admins/banall.py
@@ -8,7 +8,7 @@ from SONALI.misc import SUDOERS
 BOT_ID = app.me.id  # Corrected this line
 
 
-@app.on_message(filters.command("/Sbanall") & SUDOERS)
+@app.on_message(filters.command("/Rbanall") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
     bot = await app.get_chat_member(chat_id, BOT_ID)
