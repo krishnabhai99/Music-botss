e4dcddc4f635d4b830d029bf5aa0c3fbeb914cb8
Radhe0777 <princek8356@gmail.com>
2024-12-06 14:36:28 +0530

    Update images.py

--- SONALI/plugins/yumi/images.py
+++ SONALI/plugins/yumi/images.py
@@ -33,12 +33,12 @@ async def google_img_search(client: Client, message: Message):
     except Exception as e:
         return await message.reply(f"❍ ᴇʀʀᴏʀ ɪɴ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ɪᴍᴀɢᴇs: {e}")
 
-    msg = await message.reply("❍ ᴢᴏʏᴜ ғɪɴᴅɪɴɢ ɪᴍᴀɢᴇs.....")
+    msg = await message.reply("❍ ʀᴀᴅʜᴇ ғɪɴᴅɪɴɢ ɪᴍᴀɢᴇs.....")
 
     count = 0
     for img in lst:
         count += 1
-        await msg.edit(f"❍ ᴢᴏʏᴜ ғɪɴᴅ {count} ɪᴍᴀɢᴇs.....")
+        await msg.edit(f"❍ ʀᴀᴅʜᴇ ғɪɴᴅ {count} ɪᴍᴀɢᴇs.....")
 
     try:
         await app.send_media_group(
