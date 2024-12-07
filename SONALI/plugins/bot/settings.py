e0f1adc988917959a0a240f9b8c49041c6e29691
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-07 11:53:44 +0530

    Update settings.py

--- SONALI/plugins/bot/settings.py
+++ SONALI/plugins/bot/settings.py
@@ -55,7 +55,7 @@ async def settings_mar(client, message: Message, _):
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
         InputMediaVideo(
-            "https://envs.sh/R3-.mp4", 
+            "https://envs.sh/RYX.mp4", 
             caption= f"**Wᴇʟᴄᴏᴍᴇ ᴛᴏ {app.mention}**\n\n**Exᴘʟᴏʀᴇ ᴀ ᴡɪᴅᴇ ʀᴀɴɢᴇ ᴏғ ғᴇᴀᴛᴜʀᴇs ᴇɴʜᴀɴᴄᴇ ʏᴏᴜʀ ᴍᴜsɪᴄ ᴇxᴘᴇʀɪᴇɴᴄᴇ.\n\n**Tᴀᴘ  ᴛᴏ ɪɴᴠɪᴛᴇ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ʏᴏᴜʀ ᴏᴡɴ ɢʀᴏᴜᴘ ᴏʀ ᴄʜᴀɴɴᴇʟ**"
         ),
         reply_markup=InlineKeyboardMarkup(
