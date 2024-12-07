ec41bfd533cb5a8412b6d83b4131e3d24f92800a
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-07 11:28:49 +0530

    Update settings.py

--- SONALI/plugins/bot/settings.py
+++ SONALI/plugins/bot/settings.py
@@ -59,7 +59,9 @@ async def gib_repo(client, CallbackQuery, _):
             caption="Here's the source video you requested!"
         ),
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]           [[InlineKeyboardButton(text="• ᴍᴜsɪᴄ •", callback_data=f"settings_back_helper")]]   
+            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
+
+[[InlineKeyboardButton(text="• ᴍᴜsɪᴄ •", callback_data=f"settings_back_helper")]]   
      ),
     )
 
