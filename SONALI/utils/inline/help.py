534a953da678f5cc5c152de11d8af43c07d0bebe
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-08 17:46:11 +0530

    Update help.py

--- SONALI/utils/inline/help.py
+++ SONALI/utils/inline/help.py
@@ -59,18 +59,18 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_8"],
                     callback_data="help_callback hb8",
-                )],
-                 [
+                ),
                 InlineKeyboardButton(
                     text=_["H_B_11"],
                     callback_data="help_callback hb11",
                 ),
+               ],
+               [
                 InlineKeyboardButton(
                     text=_["H_B_12"],
                     callback_data="help_callback hb12",
                 ),
-            ],
-            [
+            
                 InlineKeyboardButton(
                     text=_["H_B_13"],
                     callback_data="help_callback hb13",
@@ -79,12 +79,6 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_14"],
                     callback_data="help_callback hb14",
                 )],
-                 [
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
-            ],
             mark,
         ]
     )
